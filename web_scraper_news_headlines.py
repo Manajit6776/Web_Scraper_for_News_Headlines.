adb559ae1468a0f5bd9397fff275b18e7a3409b0
Manajit6776 <143483396+Manajit6776@users.noreply.github.com>
2025-08-07 22:14:11 +0530

    Update web_scraper_news_headlines.py

--- web_scraper_news_headlines.py
+++ web_scraper_news_headlines.py
@@ -66,5 +66,3 @@ except requests.exceptions.RequestException as e:
     print(f"Network error: {e}")
 except Exception as e:
     print(f"An error occurred: {e}")
-
-me ie code use kia ab ek readme.
